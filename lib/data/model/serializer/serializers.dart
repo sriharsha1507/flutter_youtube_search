ce9cfeda5b6c9a598bdf2fbef6383b17451f5c0c
sriharsha1507 <sriharsha1507@gmail.com>
2019-04-24 21:06:37 -0500

    Run build runner for serializer and also added missing imports

--- lib/data/model/serializer/serializers.dart
+++ lib/data/model/serializer/serializers.dart
@@ -6,6 +6,7 @@ import 'package:built_collection/built_collection.dart';
 import 'package:built_value/built_value.dart';
 import 'package:built_value/standard_json_plugin.dart';
 import 'package:built_value/serializer.dart';
+import 'package:youtube_search/data/model/search/model_search.dart';
 
 part 'serializers.g.dart';
 
