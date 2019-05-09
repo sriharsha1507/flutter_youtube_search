829adf000b6ebab029e49ee51441afb1c7137b92
sriharsha1507 <sriharsha1507@gmail.com>
2019-05-09 00:01:47 -0500

    Completed detail page

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -2,6 +2,7 @@ import 'package:kiwi/kiwi.dart' as kiwi;
 import 'package:http/http.dart' as http;
 import 'package:youtube_search/data/network/youtube_data_source.dart';
 import 'package:youtube_search/data/repository/youtube_repository.dart';
+import 'package:youtube_search/ui/detail/detail_bloc.dart';
 import 'package:youtube_search/ui/search/search_bloc.dart';
 
 void initKiwi() {
@@ -9,5 +10,6 @@ void initKiwi() {
     ..registerInstance(http.Client())
     ..registerFactory((c) => YoutubeDataSource(c.resolve()))
     ..registerFactory((c) => YoutubeRepository(c.resolve()))
-    ..registerFactory((c) => SearchBloc(c.resolve()));
+    ..registerFactory((c) => SearchBloc(c.resolve()))
+    ..registerFactory((c) => DetailBloc(c.resolve()));
 }
